5ba58fb71b7576a9a6824c70294c0b54b508dbb1
harinme <libbyyouna@gmail.com>
2024-02-07 16:07:42 +0900

    6 다중 변수 문제 시계열 7 전처리

---
+++ 07 데이터전처리.R
@@ -0,0 +1,51 @@
+z <- c(1,2,3,NA,5,NA,8)
+sum(z)
+mean(z)
+is.na(z)
+sum(is.na(z)) # true에 대해서 개수를 구해줌ㅁ
+sum(!is.na(z)) # 결측치가 아닌 것의개수를 구해줌
+sum(z,na.rm=T) # T 값에 대해서만 계산해줘 삭제하기
+
+
+z1 <- z
+z1
+z2 <- c(5,8,1,NA,3,NA,7)
+z2
+sum(is.na(z2))
+m <- as.integer(  mean(z1,na.rm = T))
+m
+z1[is.na(z1)] <- 0 # z1[TRUE] ture인 경우에만 0으로 치환됨
+z1
+
+na.omit(z2)
+
+#결측값을 제거하고 새로운 벡터 생성하기
+z3 <- as.vector(na.omit(z2))
+
+z3
+
+# 2차원 결측값 처리
+iris
+class(iris)
+x <- iris
+head(x)
+
+x[1,2] <- NA
+x[1,3] <- NA
+x[2,3] <- NA
+x[3,4] <- NA
+
+# 결측값을 찾기 위해서 열에 대한 반복으로
+for(i in 1:ncol(x)){
+  for(j in 1:ncol(x)){
+  this.na <- is.na(x[i,j])
+    if(this.na){
+      x[i,j] <- 0
+    }
+  }
+}
+head(x)
+head(this.na)
+
+
+is.na(x[1,2])
