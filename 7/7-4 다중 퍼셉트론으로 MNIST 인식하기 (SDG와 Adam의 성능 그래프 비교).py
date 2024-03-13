db3213aa04846b9245caefdadd6c27e4fc28e0ec
harinme <libbyyouna@gmail.com>
2024-03-13 17:42:45 +0900

    8장

--- 7/7-4 다중 퍼셉트론으로 MNIST 인식하기 (SDG와 Adam의 성능 그래프 비교).py
+++ 7/7-4 다중 퍼셉트론으로 MNIST 인식하기 (SDG와 Adam의 성능 그래프 비교).py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 from tensorflow.keras.models import Sequential # 학습순서 순서대로
 from tensorflow.keras.layers import Dense # 층 
-from tensorflow.keras.optimizers import SGD, Adam # 학습법 
+from tensorflow.keras.optimizers import Adam # 학습법 
 
 
 # 손글씨 데이터셋을 불러와서 학습용 데이터셋과 테스트 데이터셋으로 분리 
@@ -20,42 +20,48 @@ y_train = tf.keras.utils.to_categorical( y_train, 10)
 y_test = tf.keras.utils.to_categorical( y_test, 10 )
 
 # 2단계 모델 선택, 신경망 구조 설계 
-mlp_sgd = Sequential()
-mlp_sgd.add( Dense(units = 512, activation='tanh', input_shape = (784,)))
-mlp_sgd.add( Dense( units=10, activation='softmax'))
+dmlp = Sequential()
+dmlp.add( Dense(units = 1024, activation='relu', input_shape = (784,)))
+dmlp.add( Dense(units = 512, activation='relu' ))
+dmlp.add( Dense(units = 512, activation='relu'))
+dmlp.add( Dense( units=10, activation='softmax'))
 
 # 3단계 학습
-mlp_sgd.compile( loss='MSE', optimizer=SGD(learning_rate = 0.01),
+dmlp.compile( loss='categorical_crossentropy', 
+             optimizer=Adam(learning_rate = 0.001),
              metrics=['accuracy'])
-hist_sgd = mlp_sgd.fit( x_train, y_train, batch_size=128, epochs=50, 
-                   validation_data=(x_test, y_test), verbose=2)
 
-print('SGD 정확률=', mlp_sgd.evaluate( x_test, y_test, verbose=0)[1]*100 )
-
-# 2단계 모델 선택, 신경망 구조 설계 
-mlp_adam = Sequential()
-mlp_adam.add( Dense(units = 512, activation='tanh', input_shape = (784,)))
-mlp_adam.add( Dense( units=10, activation='softmax'))
-
-# 3단계 학습
-mlp_adam.compile( loss='MSE', optimizer=Adam(learning_rate = 0.001),
-             metrics=['accuracy'])
-hist_adam = mlp_adam.fit( x_train, y_train, batch_size=128, epochs=50, 
+hist = dmlp.fit( x_train, y_train, batch_size=128, epochs=50, 
                validation_data=(x_test, y_test), verbose=2)
+# 4단계 예측단계  정확률 
+print('Adam 정확률=', dmlp.evaluate( x_test, y_test, verbose=0)[1]*100 )
 
-print('Adam 정확률=', mlp_adam.evaluate( x_test, y_test, verbose=0)[1]*100 )
+dmlp.save( 'dmlp_trained.h5')
 
-plt.plot( hist_sgd.history['accuracy'],'r--')
-plt.plot( hist_sgd.history['val_accuracy'],'r')
-plt.plot( hist_adam.history['accuracy'],'b--')
-plt.plot( hist_adam.history['val_accuracy'],'b')
-plt.title('Comparision of SGD and Adam optimizers')
-plt.ylim( ( 0.7, 1.0)  )
+plt.plot( hist.history['accuracy'],'r--')
+plt.plot( hist.history['val_accuracy'],'r')
+
+plt.title('Accuracy graph')
+
 plt.xlabel('epochs')  #"학습횟수
 plt.ylabel('accuracy') #정확률
-plt.legend(['train_sgd', 'val_sgd', 'train_adam', 'val_adam'])
+plt.legend(['train', 'test'])
 plt.grid()
 plt.show()
+
+plt.plot( hist.history['loss'])
+plt.plot( hist.history['val_loss'])
+plt.title('Loss graph')
+plt.xlabel('epochs')  #"학습횟수
+plt.ylabel('loss') #손실
+plt.legend(['train', 'test'])
+plt.grid()
+plt.show()
+
+
+
+
+
 
 
 
